4b570abf3c538b442d1208c0f044cae96234c24e
lucaspetti <luc_petti@hotmail.com>
2020-12-23 17:57:49 +0100

    Address assignment error

--- server_test.go
+++ server_test.go
@@ -4,25 +4,21 @@ import (
 	"testing"
 	"net/http"
 	"net/http/httptest"
+	"fmt"
 )
 
 func TestGETPlayers(t *testing.T) {
 	t.Run("returns Pippen's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pippen", nil)
+		request := newGetScoreRequest("Pippen")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("returns Jordan's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Jordan", nil)
+		request := newGetScoreRequest("Jordan")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
@@ -34,4 +30,16 @@ func TestGETPlayers(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q, want %q", got, want)
+	}
 }
